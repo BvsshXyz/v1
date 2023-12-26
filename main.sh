3ebca57ae3a8ed5f53dcdb4acd2d85bbd10bf3e8
BayuTunneling <153965587+BvsshXyz@users.noreply.github.com>
2023-12-27 06:21:56 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -411,6 +411,11 @@ clear
     # / / Ambil Xray Core Version Terbaru
 latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version $latest_version
+
+    # // Setting City&Isp
+    mkdir /root/.info;curl -sS "ipinfo.io/org?token=7a814b6263b02c" > 
+    /root/.info/.isp;curl -sS "ipinfo.io/city?token=7a814b6263b02c" > 
+    /root/.info/.city
  
     # // Ambil Config Server
     wget -O /etc/xray/config.json "${REPO}config/config.json" >/dev/null 2>&1
