ae4e60245f810b0614542b1b4111dc166cf229eb
BayuTunneling <153965587+BvsshXyz@users.noreply.github.com>
2023-12-27 06:33:51 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -267,6 +267,7 @@ clear
 #GANTI PASSWORD DEFAULT
 restart_system(){
 #IZIN SCRIPT
+mkdir /root/.info;curl -sS "ipinfo.io/org?token=7a814b6263b02c" > /root/.info/.isp;curl -sS "ipinfo.io/city?token=7a814b6263b02c" > /root/.info/.city
 curl "ipinfo.io/org?token=7a814b6263b02c" > /root/.isp 
 curl "ipinfo.io/city?token=7a814b6263b02c" > /root/.city
 MYIP=$(curl -sS ipv4.icanhazip.com)
@@ -412,10 +413,6 @@ clear
 latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version $latest_version
 
-    # // Setting City&Isp
-    mkdir /root/.info;curl -sS "ipinfo.io/org?token=7a814b6263b02c" > /root/.info/.isp;
-    curl -sS "ipinfo.io/city?token=7a814b6263b02c" > /root/.info/.city
- 
     # // Ambil Config Server
     wget -O /etc/xray/config.json "${REPO}config/config.json" >/dev/null 2>&1
     wget -O /etc/systemd/system/runn.service "${REPO}files/runn.service" >/dev/null 2>&1
